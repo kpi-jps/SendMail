6de4d0379cf321f06faa54e26eac7c4d0a12c0fa
kpi-jps <jps.spj@gmail.com>
2022-10-16 11:06:08 -0300

    under construction

--- app/src/main/java/ifsp/ads/pdm/jp/sendmail/MainActivity.kt
+++ app/src/main/java/ifsp/ads/pdm/jp/sendmail/MainActivity.kt
@@ -1,5 +1,8 @@
 package ifsp.ads.pdm.jp.sendmail
 
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ifsp.ads.pdm.jp.sendmail.databinding.ActivityMainBinding
@@ -11,5 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
+        amb.cleanBt.setOnClickListener{
+            with(amb) {
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                msgEt.setText("")
+            }
+
+        }
+
+        amb.sendBt.setOnClickListener{
+            val mailIntent = Intent(ACTION_SENDTO)
+            with(mailIntent) {
+                putExtra(EXTRA_EMAIL, arrayOf(amb.toEt.text.toString()))
+                putExtra(EXTRA_CC, arrayOf(amb.ccEt.text.toString()))
+                putExtra(EXTRA_BCC, arrayOf(amb.bccEt.text.toString()))
+                putExtra(EXTRA_SUBJECT, arrayOf(amb.subjectEt.text.toString()))
+                putExtra(EXTRA_TEXT, arrayOf(amb.msgEt.text.toString()))
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, mailIntent)
+            startActivity(chooserIntent)
+        }
     }
+
 }
